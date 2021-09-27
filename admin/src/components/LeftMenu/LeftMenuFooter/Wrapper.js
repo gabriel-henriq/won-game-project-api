d7159ef5b2d317cf911587d85a559c488971f9a0
Gabriel Henrique <gabrielhcm01@gmail.com>
2021-09-27 16:11:21 -0300

    Changing powered by from Strapi to Won Games on left menu footer

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/Wrapper.js
@@ -0,0 +1,35 @@
+import styled, { css } from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Wrapper = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    width: 100%;
+    background: ${theme.main.colors.won.blue};
+    bottom: 0;
+    .poweredBy {
+      width: 100%;
+      bottom: 0;
+      height: 3rem;
+      padding-left: 15px;
+      padding-right: 15px;
+      line-height: 3rem;
+      background-color: rgba(255, 255, 255, 0.02);
+      font-size: 1rem;
+      font-weight: 400;
+      letter-spacing: 0.05rem;
+      vertical-align: middle;
+      color: ${theme.main.colors.white};
+    }
+    `}
+`;
+
+const A = styled.a`
+  &:hover {
+    color: #007bff;
+    text-decoration: underline;
+  }
+`;
+
+export default Wrapper;
+export { A };
